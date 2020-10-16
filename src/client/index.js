e247e2f58ff73cde8e05b1612bd4f3e303450b3c
Hashem <hashem.sf@gmail.com>
2020-10-16 19:16:50 +0300

    adding rendering functions

--- src/client/index.js
+++ src/client/index.js
@@ -6,9 +6,61 @@ import "./styles/footer.scss";
 import "./styles/form.scss";
 import "./styles/header.scss";
 
-console.log(checkForName);
+const store = {
+  name: "hash"
+};
 
-alert("I EXIST");
-console.log("CHANGE!!");
+// add our markup to the page
+const root = document.getElementById("root");
+
+const updateStore = (store, newState) => {
+  const newStore = Object.assign(store, newState);
+  render(root, newStore);
+};
+
+const render = async (root, state) => {
+  root.innerHTML = App(state);
+};
+
+const App = state => {
+  changeName();
+  return `
+    <header>
+        <div class="">
+            Logo
+        </div>
+        <div class="">
+            navigation
+        </div>
+    </header>
+    <main>
+        <section>
+            <form class="" onsubmit="return Client.handleSubmit(event)">
+                <input id="name" type="text" name="input" value="" placeholder="Name">
+                <input type="submit" name="" value="submit" onclick="return Client.handleSubmit(event)"
+                    onsubmit="return Client.handleSubmit(event)">
+            </form>
+        </section>
+
+        <section>
+            <strong>Form Results:</strong>
+            <div id="results"></div>
+        </section>
+    </main>
+
+    <footer>
+    <p>This is a footer</p>
+    the name is ${state.name}
+    </footer>
+  `;
+};
+
+const changeName = () => {
+  updateStore(store, { name: "sami" });
+};
+
+window.addEventListener("load", () => {
+  render(root, store);
+});
 
 export { checkForName, handleSubmit };
