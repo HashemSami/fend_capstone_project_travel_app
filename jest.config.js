7b4d2b399a36631f62864aa45786da5d82d4a930
Hashem Sami <hashem.sf@gmail.com>
2020-10-22 21:36:26 +0300

    add tests files

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  moduleFileExtensions: ["js", "jsx"],
+  moduleNameMapper: {
+    formComponent: "<rootDir>/src/client/js/formComponent.js",
+    headerComponent: "<rootDir>/src/client/js/headerComponent.js",
+    resultComponent: "<rootDir>/src/client/js/resultComponent.js",
+  },
+};
---
+++ src/client/__test__/headerComponent.test.js
@@ -0,0 +1,20 @@
+import { formComponent } from "formComponent";
+
+describe("App", () => {
+  it("should be able to run tests", () => {
+    const url = "url";
+    const text = "text";
+    const note = "note";
+
+    const result = `
+  <section id="usrform">
+    <form>
+        <input type="text" value="${url}" id="url" form="usrform" placeholder="Enter a page URL to analyze it's text">
+        <textarea rows="6" cols="40" id="text" form="usrform" placeholder="Try your own text...">${text}</textarea>
+        <button type="button" onclick="Client.handleForm()">Analyze</button>
+    </form>
+    <div id="note">${note}</div>
+</section>`;
+    expect(formComponent(url, text, note)).toBe(result);
+  });
+});
